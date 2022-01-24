0516ae7a1583ec2e894d3c629d89878c1a71ffd1
Josh Ford <joshford@apple.com>
2022-01-24 13:13:54 -0800

    Attempted fix

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 function loadGameContent() {
     setTimeout(function() {
         var index = Math.floor(Math.random() * 10) + 1;
-        var newContent = '<div style="position: relative; display: inline;"><img src="background.jpg" class="quest-fill-image"><img src="houses/' + index + '.png" class="quest-fill-image"></div>'
+        var newContent = '<div style="position: relative; display: inline;"><img src="background.jpg" class="quest-fill-image quest-fill-image-fill"><img src="houses/' + index + '.png" class="quest-fill-image"></div>'
         
         $(newContent).appendTo('body')
         
